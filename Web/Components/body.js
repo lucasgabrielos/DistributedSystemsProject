501d3f5361343afb32dbf5ea47e8fd718a671a15
Fabio <frjcont@gmail.com>
2024-11-05 12:41:31 -0300

    Adjusting map

--- Web/Components/body.js
+++ Web/Components/body.js
@@ -2,15 +2,17 @@ import Home from './home.js';
 import Header from './header.js';
 
 const BodyComponent = {
-    components: {
-        HeaderComponent: Header, 
-        HomeComponent: Home      
-    },
-    template:`
+  components: {
+    HeaderComponent: Header,
+    HomeComponent: Home
+  },
+  template: `
       <div class=" w-full min-h-screen">
         <HeaderComponent />
         <HomeComponent />
       </div>
+       
+  
     `,
 };
 
--- Web/Components/home.js
+++ Web/Components/home.js
@@ -2,25 +2,55 @@ import Header from './header.js';
 
 const HomeComponent = {
   template: `
- <div>
-  <Header /> 
+    <div class="relative h-screen w-screen">
+     
+      <Header class="absolute top-0 left-0 w-full z-10" />
 
-  <div class="flex justify-center items-center h-screen">
-    <div class="rounded-lg h-[500px] w-[200%] lg:w-[80%] shadow-lg">
-      <iframe 
-        width="100%" 
-        height="100%" 
-        style="border:none;" 
-        loading="lazy"
-        allowfullscreen 
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.845566945619!2d-34.904949224234315!3d-8.11720209191211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1fadcaa54637%3A0xbdb1a1182c056086!2sR.%20Padre%20Carapuceiro%2C%20733%20-%20Sala%201001%20-%20Boa%20Viagem%2C%20Recife%20-%20PE%2C%2051020-280!5e0!3m2!1spt-BR!2sbr!4v1722870633793!5m2!1spt-BR!2sbr">
-      </iframe>
+      <div id="map" class="h-full w-full"></div>
     </div>
-  </div>
-</div>
-
   `,
+
+  mounted() {
+
+    const map = L.map('map', {
+      center: [-8.117202, -34.904949],
+      zoom: 13,
+      zoomControl: false,
+    });
+
+
+    var map1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    map1.addTo(map);
+
+    var hot = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    });
+
+   
+    const singleMarker = L.marker([-8.117202, -34.904949]);
+
+    var baseMaps = {
+      "Osm": map1,
+      "Humanitarian": hot
+    };
+
+    var overlayMaps = {
+      "Marker": singleMarker
+    };
+
+    L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
+  
+    singleMarker.addTo(map);
+
+
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 100);
+  },
 };
 
 export default HomeComponent;
